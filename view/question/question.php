c760ee6e53a0b1a4adcd328366fbac5cfbf3906a
Elena Perers <elena.perers@chasacademy.se>
2020-01-11 18:43:06 +0100

    Added answers to the view

--- view/question/question.php
+++ view/question/question.php
@@ -31,7 +31,7 @@ endif;
         </div>
         <p><?= $question->text; ?></p>
         <div class="user user-right">
-            <a href="user/userprofile/<?= $question->user->id ?>">
+            <a href="../../user/userprofile/<?= $question->user->id ?>">
                 <div class="user-img">
                     <img src="<?= $question->user->image; ?>" alt="<?= $question->user->name; ?>">
                 </div>
@@ -53,4 +53,36 @@ endif;
         endif;
         ?>
     </div>
+</div>
+<div class="question-container answer-container">
+    <h4>Answers</h4>
+    <?php if ($question->answers) : ?>
+        <?php foreach ($question->answers as $answer) : ?>
+            <p>Created <?= date('Y/m/d H:i:s', $answer->created); ?></p>
+            <div class="question">
+                <div class="score">
+                    <button class="vote-up"><i class="up"></i></button>
+                    <div class="vote-count"></div>
+                    <button class="vote-down"><i class="down"></i></button>
+                </div>
+                <p><?= $answer->text; ?></p>
+                <div class="user user-right">
+                    <a href="../../user/userprofile/<?= $answer->user->id ?>">
+                        <div class="user-img">
+                            <img src="<?= $answer->user->image; ?>" alt="<?= $answer->user->name; ?>">
+                        </div>
+                        <div class="user-small">
+                            <h3><?= $answer->user->name; ?></h3>
+                            <p>Score: <?= $answer->user->score; ?></p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="profileBtn">
+                <a href="../../answer/update/<?= $answer->id ?>">Edit answer</a>
+            </div>
+        <?php endforeach; ?>
+    <?php
+    endif;
+    ?>
 </div>
