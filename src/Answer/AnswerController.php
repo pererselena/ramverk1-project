027fa4cfe98daecc6485c38fc03c4fa4791d5a91
Elena Perers <elena.perers@chasacademy.se>
2020-01-13 17:40:41 +0100

    Fixed bugs and delete actions

--- src/Answer/AnswerController.php
+++ src/Answer/AnswerController.php
@@ -107,7 +107,7 @@ class AnswerController implements ContainerInjectableInterface
      *
      * @return object as a response object
      */
-    public function updatecommentAction(int $aid): object
+    public function updatecommentAction(int $id): object
     {
         if (!$this->isLoggedIn()) {
             return $this->di->response->redirect("user/login");
@@ -115,19 +115,35 @@ class AnswerController implements ContainerInjectableInterface
         $page = $this->di->get("page");
         $answer = new Answer();
         $answer->setDb($this->di->get("dbqb"));
-        $qid = $answer->findById($aid)->qid;
-        $form = new UpdateCommentForm($this->di, $aid, $qid);
-        $form->check();
+        $ans = $answer->findById($id);
+        $qid = $answer->findById($ans->id)->qid;
+        $comment = new AComment();
+        $comment->setDb($this->di->get("dbqb"));
+        $comment->findById($id);
+        $session = $this->di->get("session");
+        $userId = $session->get("userId");
+        if ((int) $comment->uid == (int) $userId) {
+            $form = new UpdateCommentForm($this->di, $id, $qid);
+            $form->check();
 
+            $page->add("anax/v2/article/default", [
+                "content" => $form->getHTML(),
+            ]);
+            var_dump($userId);
+            var_dump($ans->uid);
+
+            return $page->render([
+                "title" => "A update comment page",
+            ]);
+        }
         $page->add("anax/v2/article/default", [
-            "content" => $form->getHTML(),
+            "content" => "OOPS!!! You are not the owner of this comment!",
         ]);
 
         return $page->render([
-            "title" => "A update comment page",
+            "title" => "Error",
         ]);
     }
-
     /**
      * Description.
      *
@@ -177,7 +193,7 @@ class AnswerController implements ContainerInjectableInterface
         $answer = new Answer();
         $answer->setDb($this->di->get("dbqb"));
         $ans = $answer->findById($id);
-        if ($ans->uid == $userId) {
+        if ((int)$ans->uid == (int)$userId) {
             $form = new UpdateAnswerForm($this->di, $id);
             $form->check();
 
@@ -223,7 +239,7 @@ class AnswerController implements ContainerInjectableInterface
         $question = new Question();
         $question->setDb($this->di->get("dbqb"));
         $question->findById($ans->qid);
-        if ($question->uid == $userId) {
+        if ((int)$question->uid == (int)$userId) {
             if ((int)$ans->accepted == 0) {
                 $ans->unsetAccepted($this->di);
                 $ans->accepted = true;
@@ -268,7 +284,7 @@ class AnswerController implements ContainerInjectableInterface
         $answer = new Answer();
         $answer->setDb($this->di->get("dbqb"));
         $answer->findById($id);
-        if ($answer->uid !== $userId) {
+        if ((int)$answer->uid !== (int)$userId) {
             $answer->score += $vote;
             $user->findById($userId);
             $user->votes += 1;
@@ -315,7 +331,7 @@ class AnswerController implements ContainerInjectableInterface
         $answer = new Answer();
         $answer->setDb($this->di->get("dbqb"));
         $answer->findById($comment->aid);
-        if ($comment->uid !== $userId) {
+        if ((int)$comment->uid !== (int)$userId) {
             $comment->score += $vote;
             $user->findById($userId);
             $user->votes += 1;
@@ -328,6 +344,79 @@ class AnswerController implements ContainerInjectableInterface
         }
         $page->add("anax/v2/article/default", [
             "content" => "OOPS!!! You are the owner of this comment! Cheater!!!",
+        ]);
+
+        return $page->render([
+            "title" => "Error",
+        ]);
+    }
+
+    /**
+     * Description.
+     *
+     * @param datatype $variable Description
+     *
+     * @throws Exception
+     *
+     * @return object as a response object
+     */
+    public function deleteAction(int $id): object
+    {
+        if (!$this->isLoggedIn()) {
+            return $this->di->response->redirect("user/login");
+        }
+        $page = $this->di->get("page");
+        $user = new User();
+        $user->setDb($this->di->get("dbqb"));
+        $session = $this->di->get("session");
+        $userId = $session->get("userId");
+        $answer = new Answer();
+        $answer->setDb($this->di->get("dbqb"));
+        $answer->findById($id);
+        if ((int) $answer->uid == (int) $userId) {
+            $answer->removeWithComments($this->di);
+            return $this->di->response->redirect("questions/question/$answer->qid");
+        }
+        $page->add("anax/v2/article/default", [
+            "content" => "OOPS!!! You are not the owner of this answer!",
+        ]);
+
+        return $page->render([
+            "title" => "Error",
+        ]);
+    }
+
+    /**
+     * Description.
+     *
+     * @param datatype $variable Description
+     *
+     * @throws Exception
+     *
+     * @return object as a response object
+     */
+    public function deletecommentAction(int $id): object
+    {
+        if (!$this->isLoggedIn()) {
+            return $this->di->response->redirect("user/login");
+        }
+        $page = $this->di->get("page");
+        $user = new User();
+        $user->setDb($this->di->get("dbqb"));
+        $session = $this->di->get("session");
+        $userId = $session->get("userId");
+        $aComment = new AComment();
+        $aComment->setDb($this->di->get("dbqb"));
+        $aComment->findById($id);
+        $answer = new Answer();
+        $answer->setDb($this->di->get("dbqb"));
+        $answer->findById($aComment->aid);
+        if ((int) $aComment->uid == (int) $userId) {
+            $aComment->delete();
+            return $this->di->response->redirect("questions/question/$answer->qid");
+        }
+        $page->add("anax/v2/article/default", [
+            "content" => "OOPS!!! You are not the owner of this comment!",
         ]);
 
         return $page->render([
